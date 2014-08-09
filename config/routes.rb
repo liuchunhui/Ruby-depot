d90a83e3180a2969d26160e285af9ae53a03ab7e
liuchunhui <chunhui_liu@163.com>
2014-08-09 10:50:29 +0800

    创建购物车

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
   get 'store/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
